260b291d6f467e081ae910d1143d8bab5134c763
Maryjanechukelu <maryjanechukelu@gmail.com>
2024-06-13 15:41:45 +0100

    added playerID and game chips

--- BlackJack Game/Tutorial3.js
+++ BlackJack Game/Tutorial3.js
@@ -1,40 +1,106 @@
-let firstCard = 10
-let secondCard = 7
-let sum = firstCard + secondCard
-let cards = [firstCard, secondCard]
+let sum = 0
+let cards = []
 let message = ""
+isAlive = false
+hasBlackjack = false
 
 let sumUP = document.getElementById("sums")
 let messAGE = document.getElementById("messages")
 let carD = document.getElementById("cards")
 
+let player = {
+  name: "Jane",
+  chips: 145,
+}
+
+let playerInfo = document.getElementById("players")
+playerInfo.textContent = player.name + ": $" + player.chips
+
+function getRandomCard() {
+  let randomNumber = Math.floor(Math.random() * 13) + 1
+  if (randomNumber > 10) {
+    return 10
+  } else if (randomNumber === 1) {
+    return 11
+  } else {
+    return randomNumber
+  }
+}
 
 function startGame() {
-    playGame()
+  let firstCard = getRandomCard()
+  let secondCard = getRandomCard()
+  let sum = firstCard + secondCard
+  let cards = [firstCard, secondCard]
+  isAlive = true
+  playGame()
 }
 
 function playGame() {
+  sumUP.textContent = "Sum:" + " " + sum
+  carD.textContent = "Cards: "
+  for (let i = 0; i < cards.length; i++) {
+    carD.textContent += cards[i] + " "
+  }
+  if (sum <= 20) {
+    message = "Do you want to draw a new card?"
+  } else if (sum === 21) {
+    message = "You have got a blackjack!"
+    hasBlackjack = true
+  } else {
+    message = "You are out of the game!"
+    isAlive = false
+  }
 
-    sumUP.textContent = "Sum:" + " " + sum
-    carD.textContent = "Cards:" + " " + cards[0] + " " + cards[1]
-    if  (sum <= 20) {
-        message = "Do you want to draw a new card?"
-    } else if (sum === 21){
-        message = "You have got a blackjack!"
-    } else {
-        message = "You are out of the game!"
-    }
-
-    messAGE.textContent = message
+  messAGE.textContent = message
 }
 
 function newCard() {
-    let card = 3 
-
+  if (isAlive === true && hasBlackjack === false) {
+    let card = getRandomCard()
     sum += card
-
+    cards.push(card)
     playGame()
-
+  }
 }
 
- 
+/*
+for (let x = 10; x < 101; x += 10) {
+    console.log(x)
+}
+
+               drawing random numbers in javascript and calling it in a function
+
+function rollDice() {
+
+  let randomNumber = Math.floor(Math.random() * 6) + 1
+    return randomNumber
+
+}
+ console.log(rollDice())
+
+                Logical Operators &&
+
+let hasSolvedChallenge = false
+let hasHintsLeft = false
+
+if (hasSolvedChallenge === false && hasHintsLeft === false) {
+  showSolution()
+}
+
+function showSolution() {
+  console.log("Showing solutions....")
+}
+
+                Logical Operators OR
+
+let likesDocumenteries = true
+let likesStartups = true
+
+if (likesDocumenteries === true || likesStartups === false) {
+  recommendMovies()
+}
+
+function recommendMovies() {
+  console.log("Hey, Check out this new movies we think you may like")
+} */
